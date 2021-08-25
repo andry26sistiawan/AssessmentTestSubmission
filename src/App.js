368e02beb0dc6833feaaa9937889d6107823d8f0
Andry Sistiawan <andry.sistiawan@poinin.com>
2021-08-25 21:03:38 +0700

    Action Get List Repos

--- src/App.js
+++ src/App.js
@@ -2,13 +2,37 @@ import './app.scss';
 import { Component } from 'react';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataListRepos: [],
+    };
+  }
+
+  async handleGetListRepos(){
+    await this.getListRepos();
+  }
+
+  async getListRepos(){
+    let result = await fetch('https://api.github.com/users/andry26sistiawan/repos?per_page=100', {
+      method: 'GET',
+      headers: {},
+    }); 
+    let resultJson = await result.json();
+    this.setState({
+      dataListRepos : resultJson
+    });
+
+  }
+
   render(){
     return(
       <div className="main-page">
         <div className="header">
-          <div className="menu">
-            <p>Get List MyRepositories </p>
-          </div>
+          <button className="btn-getListRepos" onClick={()=>{this.getListRepos()}}>
+            <p>Get My Repositories </p>
+          </button>
 
         </div>
         
